bb5d7cc4c2bc9fce81f103a129788ff6f5157016
davidgomez <marketplacepremiumdev@gmail.com>
2025-10-28 18:45:18 -0500

    add filterset for manager profile

--- accounts/filtersets.py
+++ accounts/filtersets.py
@@ -1,6 +1,6 @@
 from django_filters import FilterSet, CharFilter
 
-from accounts.models import CollectorProfile
+from accounts.models import CollectorProfile, ManagerProfile
 
 
 class CollectorProfileFilterset(FilterSet):
@@ -11,6 +11,19 @@ class CollectorProfileFilterset(FilterSet):
 
     class Meta:
         model = CollectorProfile
+        fields = {
+            "is_active": ["exact"],
+        }
+
+
+class ManagerProfileFilterset(FilterSet):
+    full_name = CharFilter(
+        field_name="user__full_name",
+        lookup_expr="icontains"
+    )
+
+    class Meta:
+        model = ManagerProfile
         fields = {
             "is_active": ["exact"],
         }
